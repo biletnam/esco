29a75389cb26f4cbf1211cebcee22f6a6467471b
Lars Novikov <larsnovikov@yandex.ru>
2017-12-03 12:41:25 +0300

    iss-1 модели домен и сайт

---
+++ common/models/Domain.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "domain".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $site_id
+ * @property integer $is_main
+ */
+class Domain extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'domain';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['site_id', 'is_main'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'site_id' => 'Site ID',
+            'is_main' => 'Is Main',
+        ];
+    }
+}
